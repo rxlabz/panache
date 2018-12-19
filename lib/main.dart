024097e9da1699b2b314dcdc2cf5824e7289bc02
Erick Ghaumez <rxlabz@gmail.com>
2018-12-19 01:39:52 +0100

    feature: add a ChipTheme editor + preview

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,29 @@ import 'package:flutterial/flutterial.dart';
 import 'package:flutterial_components/flutterial_components.dart';
 import 'package:scoped_model/scoped_model.dart';
 
+const primarySwatch = Colors.blueGrey;
+
+final _flutterialTheme = ThemeData.localize(
+  ThemeData(
+    primarySwatch: primarySwatch,
+    textTheme: Typography.blackCupertino.copyWith(
+      subtitle: Typography.blackCupertino.subtitle
+          .copyWith(color: primarySwatch.shade400),
+      title: Typography.blackCupertino.title
+          .copyWith(color: primarySwatch.shade300),
+      headline:
+          Typography.blackCupertino.headline.copyWith(color: primarySwatch),
+    ), /*
+    primaryTextTheme: Typography.englishLike2018.copyWith(
+        subtitle: Typography.englishLike2018.subtitle
+            .copyWith(color: primarySwatch.shade400)),
+    accentTextTheme: Typography.englishLike2018.copyWith(
+        subtitle: Typography.englishLike2018.subtitle
+            .copyWith(color: primarySwatch.shade400)),*/
+  ),
+  Typography.blackCupertino,
+);
+
 void main() {
   final themeModel = ThemeModel(service: ThemeService());
 
@@ -11,6 +34,7 @@ void main() {
     ScopedModel<ThemeModel>(
       model: themeModel,
       child: MaterialApp(
+        theme: _flutterialTheme,
         home: Scaffold(
           body: FlutterialApp(),
         ),
