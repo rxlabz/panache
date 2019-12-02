6b2eb7e068e076634236654b97e376a88cee965f
rxlabz <erick@rxlabz.com>
2019-12-02 13:57:10 +0100

    fix font on safari

--- panache_mobile/ios/Flutter/flutter_export_environment.sh
+++ panache_mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/Me/dev/tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Me/dev/2019/lab/panache/panache"
+export "FLUTTER_ROOT=/Users/rxlabz/dev/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rxlabz/dev/projects/panache/dev/panache/panache_mobile"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/Me/dev/tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rxlabz/dev/tools/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.2.2"
 export "FLUTTER_BUILD_NUMBER=0.2.2"
